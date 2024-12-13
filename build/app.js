49e3dd7098ae34fe63650d3a67b6e5c1c30f1fef
Lewis <lewis.burgess314@gmail.com>
2024-12-13 12:58:14 +0000

    Update production build

--- build/app.js
+++ build/app.js
@@ -10,6 +10,7 @@ const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
+app.use(express_1.default.static('dist'));
 app.use('/ping', (_req, res) => {
     res.send('pong');
 });
